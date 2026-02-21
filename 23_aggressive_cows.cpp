837c70d8bb058e58ac02c4a05cb4523bf10a27aa
rayyanak23 <rayyanstat09@gmail.com>
2026-02-21 07:56:46 +0530

    aggresive cows

---
+++ 23_aggressive_cows.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+bool isPossible(vector<int> &arr, int n, int c,int minAllowedDist){ // O(n)
+    int cows = 1, lastCowPos = arr[0];
+
+    for(int i = 1; i < n; i++){
+        if(arr[i]-lastCowPos >= minAllowedDist){
+            cows++, lastCowPos = arr[i];
+        }
+        if(cows == c){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int getDistance(vector<int> &arr, int n, int c){
+    sort(arr.begin(), arr.end()); // O(nlogn)
+
+    int st = 1, end = arr[n-1]-arr[0];
+
+
+    int ans = -1;
+    while(st <= end){ //O(log(range)*n)
+        int mid = st + (end-st)/2;
+        if(isPossible(arr,n,c,mid)){
+            ans = mid;
+            st = mid+1;
+        }else{
+            end = mid-1;
+        }
+    }
+
+    return ans;
+
+
+}
+
+int main(){
+    int n=5, c=3;
+    vector<int> arr = {1,2,8,4,9};
+    cout << getDistance(arr,n,c)<< endl;
+    return 0;
+}
