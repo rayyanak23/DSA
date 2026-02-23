2cc365a2df7d9a20c54093b64b3c4228c2ca1dc3
rayyanak23 <rayyanstat09@gmail.com>
2026-02-24 00:06:33 +0530

    string compression

---
+++ 33_string compression.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int compress(vector<char>& chars) {
+    int n = size(chars);
+    int idx = 0;
+    for(int i = 0; i < n;){
+        char ch = chars[i];
+        int count = 0;
+        while(i<n && chars[i] == ch){
+            count ++; i++;
+        }
+
+        if(count == 1){
+            chars[idx++] = ch;
+        }else{
+            chars[idx++] = ch;
+            string str = to_string(count);
+            for(char dig : str){
+                chars[idx++] = dig;
+            }
+        }
+
+            // can also use chars.resize()
+    }
+
+    return idx; // equals to nums of elements added 
+}
