5507867861aca361be3cfeca3f6a6d1a89832964
rayyanak23 <rayyanstat09@gmail.com>
2026-03-09 23:24:59 +0530

    day 8

---
+++ march/8_leet3129.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        const int MOD = 1e9 + 7;
+
+        vector<vector<long long>> dp0(zero + 1, vector<long long>(one + 1));
+        vector<vector<long long>> dp1(zero + 1, vector<long long>(one + 1));
+
+        for (int i = 1; i <= min(zero, limit); i++)
+            dp0[i][0] = 1;
+
+        for (int j = 1; j <= min(one, limit); j++)
+            dp1[0][j] = 1;
+
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+
+                for (int k = 1; k <= limit && k <= i; k++) {
+                    dp0[i][j] = (dp0[i][j] + dp1[i-k][j]) % MOD;
+                }
+
+                for (int k = 1; k <= limit && k <= j; k++) {
+                    dp1[i][j] = (dp1[i][j] + dp0[i][j-k]) % MOD;
+                }
+            }
+        }
+
+        return (dp0[zero][one] + dp1[zero][one]) % MOD;
+    }
+};
